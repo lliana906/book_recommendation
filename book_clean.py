d428162c861968ac8a1e22051373d31871da0401
kimyujeong <kkgt0217@gmail.com>
2026-06-11 16:14:15 +0900

    ㅇㄴㅁ

--- book_clean.py
+++ book_clean.py
@@ -1,1 +1,7 @@
 import pandas as pd
+df = pd.read_csv('data/yes24_steadyseller_장르통합.csv')
+df = df[['ISBN','장르']]
+df['ISBN'] = df['ISBN'].astype('Int64')
+
+df.info()
+df.to_csv('data/yes24_steadyseller_장르.csv', index=False)
